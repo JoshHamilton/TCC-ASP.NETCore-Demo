96512b386f071bda60e884db3180f4b7a6b9c80f
jhamilton <me@joshhamilton.org>
2025-02-11 15:59:28 -0800

    Create a class for school Classes

---
+++ Models/Class.cs
@@ -0,0 +1,11 @@
+namespace WebAppDemo.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string ClassName { get; set; } = string.Empty;
+        public string ClassDescription { get; set; } = string.Empty;
+        public decimal Units { get; set; }
+        public string Quarter { get; set; } = string.Empty;
+    }
+}
