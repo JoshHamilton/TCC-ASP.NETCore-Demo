b7e4ed55dea0dbb1b9735096a2d1d705758da4d9
jhamilton <me@joshhamilton.org>
2025-02-11 17:47:47 -0800

    Require > 0 in Units and set required in model

--- Models/Class.cs
+++ Models/Class.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebAppDemo.Models
 {
     public class Class
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Class Name is required.")]
         public string ClassName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Class Description is required.")]
         public string ClassDescription { get; set; } = string.Empty;
+        [Range(0.1, double.MaxValue, ErrorMessage = "Units must be greater than 0.")]
         public decimal Units { get; set; }
+        [Required(ErrorMessage = "Quarter is required.")]
         public string Quarter { get; set; } = string.Empty;
     }
 }
