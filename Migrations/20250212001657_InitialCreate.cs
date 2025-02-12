203698e0cc9d8b23de0d0b58c113360ca2a5c0c8
jhamilton <me@joshhamilton.org>
2025-02-11 16:21:46 -0800

    Add the migrations. Database tables setup

---
+++ Migrations/20250212001657_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAppDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Classes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ClassName = table.Column<string>(type: "TEXT", nullable: false),
+                    ClassDescription = table.Column<string>(type: "TEXT", nullable: false),
+                    Units = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Quarter = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Classes");
+        }
+    }
+}
