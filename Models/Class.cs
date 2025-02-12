fd25abc01cf22fbd3440e3f9bb37562310e122e3
jhamilton <me@joshhamilton.org>
2025-02-11 17:58:12 -0800

    Improve form validation display text

--- Models/Class.cs
+++ Models/Class.cs
@@ -6,13 +6,17 @@ namespace WebAppDemo.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "Class Name is required.")]
+        [Display(Name = "Class Name")]
         public string ClassName { get; set; } = string.Empty;
         [Required(ErrorMessage = "Class Description is required.")]
+        [Display(Name = "Class Description")]
         public string ClassDescription { get; set; } = string.Empty;
-        [Range(0.1, double.MaxValue, ErrorMessage = "Units must be greater than 0.")]
+        [Range(0.1, double.MaxValue, ErrorMessage = "Units must be greater than 0.1.")]
+        [Display(Name = "Units")]
         public decimal Units { get; set; }
         [Required(ErrorMessage = "Quarter is required.")]
         [RegularExpression("Fall|Winter|Spring|Summer", ErrorMessage = "Quarter must be one of the following: Fall, Winter, Spring, Summer.")]
+        [Display(Name = "Quarter")]
         public string Quarter { get; set; } = string.Empty;
     }
 }
